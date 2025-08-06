2a9a12d4bef617714352cca448da8317a780c60c
mrwangshipei <139351222+mrwangshipei@users.noreply.github.com>
2025-08-06 13:43:53 +0800

    first
    first commit

---
+++ Model/CountModel - 复制.cs
@@ -0,0 +1,17 @@
+﻿using Kernal;
+using UPPERIOC.UPPER.IOC.Annaiation;
+using UPPERIOC2.UPPER.UFileModel.Model;
+
+namespace FCT.Model
+{
+	public class X 
+	{
+		public DictionaryEx<string, PNCountModel> PNCounts { get; set; }
+
+
+
+		// 字典项的包装类
+	}
+
+	
+}
